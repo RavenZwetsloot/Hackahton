41a097a491ec861efeba1ad36eb60d1a2b4b62f4
RavenZwetsloot <rave.nzwetsloot@hotmail.com>
2018-11-29 08:41:36 +0100

    added

---
+++ livezilla/_config/config.php
@@ -0,0 +1,24 @@
+<?php
+
+/*********************************************************************************
+* LiveZilla config.php
+*
+* Copyright 2016 LiveZilla GmbH
+* All rights reserved.
+* LiveZilla is a registered trademark.
+*
+********************************************************************************/
+
+$_CONFIG['gl_pr_cr'] = '1543395804';
+$_CONFIG['gl_lzid'] = '186caf0ce6';
+$_CONFIG['b64'] = false;
+
+$_CONFIG[0]['gl_db_host'] = 'localhost';
+$_CONFIG[0]['gl_db_user'] = 'root';
+$_CONFIG[0]['gl_db_ext'] = 'mysqli';
+$_CONFIG[0]['gl_db_eng'] = 'MyISAM';
+$_CONFIG[0]['gl_db_pass'] = '';
+$_CONFIG[0]['gl_db_name'] = '4mechat';
+$_CONFIG[0]['gl_db_prefix'] = 'lz_';
+
+?>
